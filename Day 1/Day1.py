92a7fda1e26ce549c212034154d791aff267cc74
Satyam Rana <57563008+satyamranatc@users.noreply.github.com>
2024-03-20 17:42:43 +0530

    first commit

---
+++ Day 1/Day1.py
@@ -0,0 +1,47 @@
+# print("Hello")
+# a = 5 # Variables
+# print("The Result is",a+5)
+
+#----------------------------------------------------------------
+
+a:int # Variables Declaration
+a = 5 # Variable Initialization
+# print(a)
+
+MyDataInThis = 4
+myDataInThis = 4
+my_Data_In_This = 4
+
+#----------------------------------------------------------------
+
+# print("Hi",end="")
+# print("Hello This is Python")
+print("Hello\n" * 3)
+#----------------------------------------------------------------
+
+# x = 55
+# y = 6
+
+# print(x + y)
+# print(x - y)
+# print(x * y)
+# print(x / y)
+# print(x // y)
+# print(x % y)
+# print(x ** y)
+
+# print(x > y)
+# print(x < y)
+# print(x >= y)
+# print(x <= y)
+# print(x == y)
+# print(x != y)
+
+m = 5
+m **= 4
+print(m)
+
+
+
+
+
